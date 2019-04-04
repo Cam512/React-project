b36eb0dd2ccbfa59587017d8486e574d41999977
Cameron Horejs <cameron.horejs@gmail.com>
2019-04-03 22:01:21 -0700

    added useContext hook in Cockpit component

--- src/Components/Cockpit/Cockpit.js
+++ src/Components/Cockpit/Cockpit.js
@@ -1,11 +1,13 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useEffect, useRef, useContext} from 'react';
 
 import classes from './Cockpit.css';
 import AuthContext from '../../context/auth-context';
 
 const cockpit = (props) => {
   const toggleBtnRef = useRef(null); 
-  
+  const authContext = useContext(AuthContext);
+
+  console.log(authContext.authenticated);
 
   useEffect(() => {
     console.log('[Cockpit.js] useEffect');
@@ -43,10 +45,9 @@ const cockpit = (props) => {
             <p className={assignedClasses.join(' ')}>App is running!</p>
             <button ref={toggleBtnRef}
               className={btnClass}
-              onClick={props.clicked}>Toggle persons</button>
-              <AuthContext.Consumer> 
-                {(context) => <button onClick={context.login}>Log in</button>}
-              </AuthContext.Consumer>
+              onClick={props.clicked}>Toggle persons
+              </button>
+              <button onClick={authContext.login}>Log in</button>
         </div>
     );
 };
